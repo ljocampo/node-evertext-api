f6d4bbfb91a8d92d6e48cd3920931fb721bba928
Luis Ocampo <luis.ocampo@admios-sa.com>
2018-08-24 17:23:24 -0400

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const {
 const {
   User
 } = require('./models/user');
+const {authenticate}=require('./middleware/authenticate');
 
 var app = express();
 const PORT = process.env.PORT;
@@ -114,6 +115,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(PORT, () => {
